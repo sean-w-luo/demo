e52e02cbf6e541c8e23bac25b4ca581c52edf9a3
sean-w-luo <shiwei luo@LAPTOP-2HENJFJ9>
2024-07-25 21:41:16 +0800

    有点难

--- src/week3/Exercise1.java
+++ src/week3/Exercise1.java
@@ -1,5 +1,25 @@
 package week3;
 
 public class Exercise1 {
-
+      private int number;
+      //contructor
+      public Exercise1(int num) {
+    	  number=num;
+       }
+      //accessor;
+      public int getNumber() { return number; }
+      //mutators
+      public void calculator(int number) {
+    	  
+    }
 }
+
+
+
+/*Consider a class that models a simple (non-scientific) calculator that deals only with integers. 
+ * Discuss with your colleagues what methods and variables this class might have. 
+ * Then write a Java class that would implement it.
+	One possible implementation is:
+	Variable total
+	Method add, subtract, multiply, divide, clear total, print total
+*/
