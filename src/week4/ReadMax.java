118b82238e438aa1501577d5d8beffd290ee38b4
sean-w-luo <shiwei luo@LAPTOP-2HENJFJ9.lan>
2024-07-27 19:56:40 +0800

    editing

--- src/week4/ReadMax.java
+++ src/week4/ReadMax.java
@@ -9,45 +9,57 @@ Hint:  x = Math.max(a,b); will cause x to be assigned the maximum of a and b
  Find other such methods in Math class using Java docs (ask lab-assistant)
  (c)  minimum marks：Similar to part b) but you need to use a different method
 * */
-public class ReadMax {	
-	   public static void main(String[] args) {
-		      int total=3;
-		      double a = 30.99;
-		      double b = 45.77;
-		      double c = 90.77;
-		      double x;
-		      double y;
-		      double z;
-		      double q;
-		      double avg=(a+b+c)/3;
-		      double result1;
-		   	  double result2;
-		    
-		    x= Math.max(a,b); 
-		    y=Math.max (a,c);
-		    z= Math.min(a,b);
-		    q=Math.min (a,c);
-		    
-		   public static double max(double x,double y) {
-		    	if(x>y) {
-		    		result1=x;
-		    	}else {
-		    		result1=y;  	
-		    		}		  
-		    	return result1;
-		    	}	
-		    
-		    public static double min(double x,double y) {
-		    	if(x<y) {
-		    		result2=x;
-		    	}else {
-		    		result2=y;  	
-		    		}		  
-		    	return result2;
-			}	  
-		    
-	    System.out.println(" average marks is " + avg);
-	    System.out.println("maximum mark is " + result1);
-	    System.out.println("minimum marks is " + result2);
-		}	  
+
+import java.util.*;
+
+public class ReadMax {
+
+	int total = 3;
+	double a = 30.99;
+	double b = 45.77;
+	double c = 90.77;
+	double x = Math.max(a, b);
+	double y = Math.max(a, c);
+	double z = Math.min(a, b);
+	double q = Math.min(a, c);
+
+	public void average(double a, double b, double c) {
+
+		double avg = (a + b + c) / total;
+		System.out.println(" average marks is " + avg);
+	}
+
+	// max方法；
+	public static double max(double x, double y) {
+		double result1;
+		if (x > y) {
+			result1 = x;
+		} else {
+			result1 = y;
+		}
+		return result1;
+	}
+
+//min方法
+	public static double min(double z, double q) {
+		double result2;
+		if (z < q) {
+			result2 = z;
+		} else {
+			result2 = q;
+		}
+		return result2;
+	}
+
+	public static void main(String[] args) {
+		
+		double result3;
+		double result4;
+		
+		double result3=ReadMax.max();
+		double result4=ReadMax.min(); 
+
+		System.out.println("maximum mark is " + result3);
+		System.out.println("minimum marks is " + result4);
+	}
 }
