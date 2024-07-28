122ccb9a7efe7b11d34e8bd1ea0d7a6d64d611ad
sean-w-luo <shiwei luo@LAPTOP-2HENJFJ9.lan>
2024-07-28 13:34:02 +0800

    update

--- src/week5/Validate.java
+++ src/week5/Validate.java
@@ -1,5 +1,19 @@
 package week5;
 
+import java.util.*;
 public class Validate {
+	 public static void main(String args[]){ 
+	    Scanner console=new Scanner(System.in);
+        String name; 
+	     char gender;
+	      System.out.print("Enter your name : ");
+	       name = console.nextLine();
+	       do {System.out.print("Enter gender (M/F) :");  
+	          gender = console.nextLine().charAt(0);  		  	
+	       } while ( gender != "M" && gender != "F");
+		if ( gender == "M")
+	          System.out.print("Welcome Mr. " + name);
+	       else System.out.print("Welcome Mr. " + name);
+	   }
 
 }
